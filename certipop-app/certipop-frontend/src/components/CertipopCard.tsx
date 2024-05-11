69fe8e93da0e4407b48713ca37b483dcc9b4f32a
Solomon WE₿3 <124439840+lemusosa@users.noreply.github.com>
2024-05-11 09:49:58 +0200

    fix: text-overflow on card Update CertipopCard.tsx

--- certipop-app/certipop-frontend/src/components/CertipopCard.tsx
+++ certipop-app/certipop-frontend/src/components/CertipopCard.tsx
@@ -10,15 +10,15 @@ const CertipopCard: FC<CardProps> = ({ certipop }) => {
 
     return (
         <div className="relative group rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30 m-4">
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-                {transactionReference.toUpperCase()}{" "}
+            <h2 className={`mb-3 text-2xl font-semibold text-ellipsis overflow-hidden`}>
+                {transactionReference}{" "}
             </h2>
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                {transactionSignature}
+                {transactionSignature.toUpperCase()}
             </p>
             <p
                 className={`mt-6 max-w-[30ch] text-sm opacity-75`}
-            >{`${rating}/5`}</p>
+            >{`${rating}/10`}</p>
         </div>
     );
 };
