bcfb8ff36f95b159c27f0b4e6d5ff61c78e726b1
Solomon WE₿3 <124439840+lemusosa@users.noreply.github.com>
2024-05-11 10:34:56 +0200

    link: add clickable certipop id in card Update CertipopCard.tsx

--- certipop-app/certipop-frontend/src/components/CertipopCard.tsx
+++ certipop-app/certipop-frontend/src/components/CertipopCard.tsx
@@ -9,9 +9,11 @@ const CertipopCard: FC<CardProps> = ({ certipop }) => {
     const { transactionReference, transactionSignature, rating } = certipop;
 
     return (
-        <div className="relative group rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30 m-4">
+        <div className="relative group rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-300 
+        bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30 m-4">
             <h2 className={`mb-3 text-2xl font-semibold text-ellipsis overflow-hidden`}>
-                {transactionReference}{" "}
+                <a href={`https://explorer.solana.com/tx/${transactionReference}?cluster=devnet`} target="_blank" 
+                className="font-medium text-inherit-600 dark:text-inherit-500 hover:underline">Certipop ID: {transactionReference}</a>
             </h2>
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
                 {transactionSignature.toUpperCase()}
